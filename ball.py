872c8d556bf9b2ac5052a83ded3b48aa16e273f1
Harshal <harshalpatel2193@gmail.com>
2023-09-29 12:20:46 +0530

    feature - ball can counce off player

--- ball.py
+++ ball.py
@@ -5,7 +5,7 @@ import math
 class Ball(pg.sprite.Sprite):
 
 
-    def __init__(self, color):
+    def __init__(self, color,player_1_sprite,player_2_sprite):
         pg.sprite.Sprite.__init__(self)
 
         self.radius = 10
@@ -20,16 +20,26 @@ class Ball(pg.sprite.Sprite):
        
 
         self.position = pg.Vector2((SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-        self.velocity = pg.Vector2((self.speed,self.speed))
+        self.velocity = pg.Vector2((self.speed*2,self.speed))
         
         self.new_x = SCREEN_WIDTH/2
         self.new_y = SCREEN_HEIGHT/2
 
+        self.player_1_sprite = player_1_sprite
+        self.player_2_sprite = player_2_sprite
+
+        self.player_1_collide = False
+        self.player_2_collide = False
+
+    def collision_sprite(self, ball, player):
+        if pg.sprite.collide_rect(ball,player):
+            return True
+
+        else: return False    
 
     def moving(self):
         
-        self.position += self.velocity
-        self.rect.center = self.position
+        # simply bounce
         
         if self.position.y > SCREEN_HEIGHT-10 or self.position.y < 10:
             self.velocity.y = -self.velocity.y
@@ -37,6 +47,19 @@ class Ball(pg.sprite.Sprite):
         if self.position.x > SCREEN_WIDTH-10 or self.position.x < 10:
             self.velocity.x = -self.velocity.x
 
+        # player collisions
+        if self.player_1_collide and self.position.x >= PLAYERS_X_POS[0]:
+            self.velocity.x = - self.velocity.x
+            self.position.x += 3
+        
+        if self.player_2_collide and self.position.x <= PLAYERS_X_POS[1]: 
+            self.velocity.x = - self.velocity.x
+            self.position.x -= 3
+
+        self.position += self.velocity
+        self.rect.center = self.position
 
     def update(self,dt):
         self.moving()
+        self.player_1_collide = self.collision_sprite(self, self.player_1_sprite)
+        self.player_2_collide = self.collision_sprite(self,self.player_2_sprite)
--- level.py
+++ level.py
@@ -12,24 +12,19 @@ class  Level :
 
         self.player_1 = Player(0)
         self.player_2 = Player(1)
-        self.ball = Ball(pg.color.Color("green"))
+        self.ball = Ball(pg.color.Color("green"),self.player_1,self.player_2)
         
         # sprites
         self.all_sprites = pg.sprite.Group(self.ball,self.player_1,self.player_2)
-    
-
-    def collision_sprite(self , ball,player):
-        if pg.sprite.collide_rect(ball,player):
-            print("Collision detected!")
+        
 
 
     def run(self ,dt):
 
         self.display_surf.fill((0, 0, 0))
        
-        self.collision_sprite(self.ball,self.player_1)
-        self.collision_sprite(self.ball,self.player_2)
         self.all_sprites.clear(self.display_surf,self.background)
         self.all_sprites.update(dt)
         self.all_sprites.draw(self.display_surf)
 
+      
